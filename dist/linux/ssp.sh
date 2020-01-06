f296fcd0ddc292285ac54bba9edc1ec67dea47d8
Hussain Taj <hussaintajwork@gmail.com>
2020-01-06 19:45:57 +0500

    Add init command to console scripts

--- dist/linux/ssp.sh
+++ dist/linux/ssp.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Check if this was an init command
+if [ $# = 1 ]
+then 
+	case $1 in
+		([Ii][Nn][Ii][Tt]) 
+		echo SSP: Running init..
+		if [ -z "$SSP_HOME_DIR" ]
+		then
+			echo SSP_HOME_DIR environment variable not defined.
+			exit 1
+		else
+			echo SSP: Copying files from $SSP_HOME_DIR
+			cp -ri "$SSP_HOME_DIR/.spark-submit-project/" "./" 
+			exit 0
+		fi
+		
+		;;
+	esac
+fi
+
 if [ -z "$SSP_PYTHON" ]
 then
 	PY_FOR_SSP=python
@@ -7,4 +27,12 @@ else
 	PY_FOR_SSP=$SSP_PYTHON
 fi
 
-$PY_FOR_SSP .spark-submit-project/spark-submit-project.py $@
+if [ -e ".spark-submit-project/spark-submit-project.py" ]
+then
+	$PY_FOR_SSP .spark-submit-project/spark-submit-project.py $@
+else
+	echo spark-submit-project/spark-submit-project.py Not Found.
+	echo     Run 
+	printf         "\tssp.sh init\n" 
+	echo     to create these files in the current directory.
+fi
--- example/windows/ssp.bat
+++ example/windows/ssp.bat
@@ -1,9 +1,54 @@
 @echo off
 
+:: count args
+SET argC=0
+FOR %%x IN (%*) DO SET/A argC+=1
+
+IF %argC% == 1 (
+
+	GOTO CheckForCommands
+) ELSE (
+
+	GOTO RunSparkSubmit
+)
+
+:CheckForCommands
+
+IF /I %1 == init (
+	GOTO InitializeSSP
+) ELSE (
+	GOTO RunSparkSubmit
+)
+
+:InitializeSSP
+
+echo SSP: Running init...
+
+IF DEFINED SSP_HOME_DIR (
+	echo SSP: Copying files from %SSP_HOME_DIR% to %cd%
+	xcopy /s "%SSP_HOME_DIR%\.spark-submit-project" ".\.spark-submit-project\"
+	GOTO end
+) ELSE (
+	echo SSP_HOME_DIR environment variable not defined.
+	exit 1
+)
+
+:RunSparkSubmit
+
+
 IF DEFINED SSP_PYTHON (
 	set PY_FOR_SSP=%SSP_PYTHON%
 ) ELSE (
 	set PY_FOR_SSP=python
 )
 
-%PY_FOR_SSP% .spark-submit-project\spark-submit-project.py %*
+IF EXIST .spark-submit-project\spark-submit-project.py (
+	%PY_FOR_SSP% .spark-submit-project\spark-submit-project.py %*
+) ELSE (
+	ECHO spark-submit-project\spark-submit-project.py Not Found.
+	ECHO     Run 
+	ECHO         "ssp.bat init" 
+	ECHO     to create these files in the current directory.
+)
+
+:end
